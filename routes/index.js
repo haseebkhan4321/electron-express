8c2eb8c9e0e3d1df629ef7542c06870d016e738f
Haseeb khan <haseeb.khan@ozteckwork.com>
2024-09-16 17:43:43 +0500

    about us in ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/about-us", function (req, res, next) {
+  res.render("about-us", { title: "About us" });
 });
 
 module.exports = router;
